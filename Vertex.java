fc7c4880abf2b1178ae47c8686aae014237b938c
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2023-04-12 22:53:35 +0530

    added vertex of the graph

---
+++ Vertex.java
@@ -0,0 +1,54 @@
+import java.util.List;
+
+public class Vertex implements Comparable<Vertex> {
+    private String name;
+    private List<Edge> adjacencyList;
+    private boolean visited;
+    private double distance;
+    private Vertex previous;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Edge> getAdjacencyList() {
+        return adjacencyList;
+    }
+
+    public void addNeighbor(Edge edge) {
+        this.adjacencyList.add(edge);
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public Vertex getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Vertex previous) {
+        this.previous = previous;
+    }
+
+    @Override
+    public int compareTo(Vertex o) {
+        return Double.compare( this.distance,o.getDistance());
+    }
+}
